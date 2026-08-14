508d41cbdcaadda169d2fe92e80a76500b20f0cd
anne sonya <anne.s@tataelxsi.co.in>
2026-08-14 16:32:55 +0530

    made changes in all the files

---
+++ README.md
@@ -0,0 +1,6 @@
+" # multibranch-python-pipeline-demo"
+
+this repo is used to demonstrate:
+-main branch
+-develop branch
+-feature-login branch
---
+++ tests/test_calculator.py
@@ -0,0 +1,16 @@
+import pytest
+from app.calculator import add,divide 
+
+@pytest.mark.smoke
+def test_add():
+    assert add(5,4)==9
+
+@pytest.mark.regression
+def test_divide():
+    assert divide(20,4)==5
+
+@pytest.mark.regression
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+     divide(10,0)
+
